2b513cdb456f3e9ed432df48a7f30439059b8d9a
Szymi13 <urbanszymon13@gmail.com>
2023-01-14 00:43:35 +0100

    wavConverter.py now creates header file for samples and .c file for storing those samples

--- python/wavConverter.py
+++ python/wavConverter.py
@@ -2,14 +2,29 @@ import wave, struct
 import numpy as np
 import os
 
-# this program saves all 16 bit wav samples to binary format
-
 dir = '../samples'
+
+for filename in os.listdir(dir):
+    file_to_write = open("../src/samples.h","a");
+    file_to_write.write("extern const int16_t " + filename[0:2] + "[];\n\n",)
+
 
 for filename in os.listdir(dir):
     file = os.path.join(dir, filename)
     wavefile = wave.open(file, 'rb')
     wavedata = wavefile.readframes(wavefile.getnframes())
     data = struct.unpack("<" + str(wavefile.getnframes()) + "h", wavedata)
-    data_array = np.asarray(data)
-    data_array.astype('int16').tofile('../src/samples_bin/' + filename[0:2] + '.bin')
+    data_arr = []
+    data_arr = np.asarray(data)
+
+    fp = open("../src/samples.c","a")
+
+    fp.write("const int16_t " + str(filename[0:2]) + ("[] = {"))
+
+    with open("../src/samples.c", "a") as fp:
+        for sample in data_arr:
+          fp.write("%i, " % sample)
+
+
+    fp = open("../src/samples.c","a")
+    fp.write("};\n\n")
--- src/samples.h
+++ src/samples.h
@@ -1,4 +1,34 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#include <stdlib.h>
 
-#endif
+extern const int16_t SD[];
+
+extern const int16_t CP[];
+
+extern const int16_t BD[];
+
+extern const int16_t HC[];
+
+extern const int16_t MA[];
+
+extern const int16_t CH[];
+
+extern const int16_t LC[];
+
+extern const int16_t RS[];
+
+extern const int16_t OH[];
+
+extern const int16_t LT[];
+
+extern const int16_t HT[];
+
+extern const int16_t CL[];
+
+extern const int16_t CB[];
+
+extern const int16_t MT[];
+
+extern const int16_t MC[];
+
+extern const int16_t CY[];
+
